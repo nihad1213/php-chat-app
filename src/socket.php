3607460d7a8cec19a0493cd27a3c309413350a03
nihad1213 <nihad.nemetli@gmail.com>
2026-03-27 11:55:34 +0400

    add:basic server created

--- src/socket.php
+++ src/socket.php
@@ -11,4 +11,42 @@ socket_listen($sock);
 
 echo "Listening for new connection on port $port \n";
 
+$members = [];
+$connections = [];
+
+$connections = [$sock];
+
+while(true) {
+    $reads = $connections;
+    $writes = $exceptions = $null;
+
+    socket_select($reads, $writes, $exceptions, 0);
+
+    if (in_array($sock, $reads)) {
+        $newConnection = socket_accept($sock);
+        $connections[] = $newConnection;
+        $reply = "connected to the chat socker server \n";
+        socket_write($newConnection, $reply, strlen($reply));
+
+        $sockIndex = array_search($sock, $reads);
+        unset($reads[$sockIndex]);
+    }
+
+    foreach($reads as $key=>$value) {
+        $data = socket_read($value, 1024);
+
+        if (!empty($data)) {
+            foreach($connections as $ckey => $cvalue) {
+                socket_write($cvalue, $data, strlen($data));
+            }
+        } else if ($data === '') {
+            echo "disconnecting client $key \n";
+            unset($connections[$key]);
+            socket_close($value);
+        }
+    }
+}
+
+
+socket_close($sock);
 
