eadacbe279a2fa5072032209f6e3e564f4077689
nihad1213 <nihad.nemetli@gmail.com>
2026-03-27 12:18:15 +0400

    new: unmask function created

--- src/functions.php
+++ src/functions.php
@@ -1,2 +1,26 @@
 <?php
 
+function unmask($text) {
+    $length = ord($text[1]) & 127;
+    if($length == 126) {
+        $masks = substr($text, 4, 4);
+        $data = substr($text, 8);
+    } elseif($length == 127) {
+        $masks = substr($text, 10, 4);
+        $data = substr($text, 14);
+    } else {
+        $masks = substr($text, 2, 4);
+        $data = substr($text, 6);
+    }
+
+    $text = '';
+    for ($i = 0; $i < strlen($data); ++$i) {
+        $text .= $data[$i] ^ $masks[$i % 4];
+    }
+
+    return $text;
+}
+
+function packData() {}
+
+function handshake() {}
